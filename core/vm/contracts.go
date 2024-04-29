f1ac7b74113a58460d8b60a022a2a75588237d05
Keefe Liu <keefe.l@users.noreply.github.com>
2024-04-29 18:49:59 +0800

    add precompiled contracts for light client

--- core/vm/contracts.go
+++ core/vm/contracts.go
@@ -127,6 +127,9 @@ var PrecompiledContractsCancun = map[common.Address]PrecompiledContract{
 	common.BytesToAddress([]byte{8}):    &bn256PairingIstanbul{},
 	common.BytesToAddress([]byte{9}):    &blake2F{},
 	common.BytesToAddress([]byte{0x0a}): &kzgPointEvaluation{},
+
+	common.BytesToAddress([]byte{102}): &blsSignatureVerify{},
+	common.BytesToAddress([]byte{103}): &cometBFTLightBlockValidate{},
 }
 
 // PrecompiledContractsBLS contains the set of pre-compiled Ethereum

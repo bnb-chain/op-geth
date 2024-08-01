fdae74b97d84f138c1e454502ccf3b77745acdee
redhdx <lredhdx@gmail.com>
2024-08-01 18:48:53 +0800

    feature(op-geth): change BundleGasLimit

--- params/protocol_params.go
+++ params/protocol_params.go
@@ -34,7 +34,7 @@ const (
 	MinGasLimit          uint64 = 5000               // Minimum the gas limit may ever be.
 	MaxGasLimit          uint64 = 0x7fffffffffffffff // Maximum the gas limit (2^63-1).
 	GenesisGasLimit      uint64 = 4712388            // Gas limit of the Genesis block.
-	BundleGasLimit       uint64 = 10000000           // Gas limit of bundle.
+	BundleGasLimit       uint64 = 80000000           // Gas limit of bundle.
 
 	MaximumExtraDataSize  uint64 = 32    // Maximum size extra data may be after Genesis.
 	ExpByteGas            uint64 = 10    // Times ceil(log256(exponent)) for the EXP instruction.

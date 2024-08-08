ff2f72752c6cff1166161928a768bc61fdeb989f
galaio <12880651+galaio@users.noreply.github.com>
2024-08-08 10:29:08 +0800

    txdag: opt read txdag file and validation logic; (#26)
    * txdag: support new txdep resolve method;
    pevm: avoid read txdag file when generating;
    
    * pevm: support read txdag file in const size;
    
    * txdag: reduce mem alloc and async resolve tx dependency;
    
    ---------
    
    Co-authored-by: galaio <galaio@users.noreply.github.com>

--- core/state/statedb.go
+++ core/state/statedb.go
@@ -2493,7 +2493,6 @@ func (s *StateDB) FinaliseRWSet() error {
 
 	// reset stateDB
 	s.rwSet = nil
-	s.stat = nil
 	return s.mvStates.FulfillRWSet(rwSet, stat)
 }
 
@@ -2567,7 +2566,7 @@ func (s *StateDB) RecordSystemTxRWSet(index int) {
 	}
 	s.mvStates.FulfillRWSet(types.NewRWSet(types.StateVersion{
 		TxIndex: index,
-	}).WithExcludedTxFlag(), types.NewExeStat(index).WithSerialFlag())
+	}).WithExcludedTxFlag(), types.NewExeStat(index).WithExcludedTxFlag())
 }
 
 // copySet returns a deep-copied set.

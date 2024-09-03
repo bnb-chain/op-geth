650d793f622c82f9792ec7a3e9ea4d2c77590c3f
galaio <galaio@users.noreply.github.com>
2024-09-03 20:32:19 +0800

    txdag: add metrics when mining;

--- miner/worker.go
+++ miner/worker.go
@@ -1078,6 +1078,9 @@ func (w *worker) generateDAGTx(statedb *state.StateDB, signer types.Signer, txIn
 	}
 	// txIndex is the index of this txDAG transaction
 	txDAG.SetTxDep(txIndex, types.TxDep{Flags: &types.NonDependentRelFlag})
+	if metrics.EnabledExpensive {
+		go types.EvaluateTxDAGPerformance(txDAG, statedb.ResolveStats())
+	}
 
 	publicKey := sender.Public()
 	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)

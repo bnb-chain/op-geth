320d565bda377762dd5cfaba3d06396f69547b7d
galaio <galaio@users.noreply.github.com>
2024-09-04 10:57:48 +0800

    txdag: fix mvstates copy issue;

--- miner/worker.go
+++ miner/worker.go
@@ -1344,6 +1344,7 @@ func (w *worker) generateWork(genParams *generateParams) *newPayloadResult {
 			wg.Add(1)
 			go func() {
 				defer wg.Done()
+				newWork.state.MVStates().EnableAsyncGen()
 				err := w.fillTransactions(interrupt, newWork)
 				if errors.Is(err, errBlockInterruptedByTimeout) {
 					log.Warn("Block building is interrupted", "allowance", common.PrettyDuration(w.newpayloadTimeout), "parentHash", genParams.parentHash)
@@ -1353,6 +1354,7 @@ func (w *worker) generateWork(genParams *generateParams) *newPayloadResult {
 					isBuildBlockInterruptCounter.Inc(1)
 				}
 			}()
+			work.state.MVStates().EnableAsyncGen()
 			err := w.fillTransactionsAndBundles(interrupt, work)
 			wg.Wait()
 			timer.Stop() // don't need timeout interruption any more
@@ -1361,6 +1363,7 @@ func (w *worker) generateWork(genParams *generateParams) *newPayloadResult {
 				work = newWork
 			}
 		} else {
+			work.state.MVStates().EnableAsyncGen()
 			err := w.fillTransactions(interrupt, work)
 			timer.Stop() // don't need timeout interruption any more
 			if errors.Is(err, errBlockInterruptedByTimeout) {

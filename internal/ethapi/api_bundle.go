fd8b604a99e807b463b62027e44a250efae25673
sysvm <112189277+sysvm@users.noreply.github.com>
2026-04-08 16:26:45 +0800

    fix: display bundle time

--- internal/ethapi/api_bundle.go
+++ internal/ethapi/api_bundle.go
@@ -6,10 +6,9 @@ import (
 	"fmt"
 	"math/big"
 
-	"github.com/ethereum/go-ethereum/log"
-
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/log"
 )
 
 const InvalidBundleParamError = -38000
